ccb87e9133be8f6bb9e6ed261ae7e8611800e3f9
Eliezer Orama <eliezerorama15@gmail.com>
2025-12-18 15:52:06 +0100

    actualizacion pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://eorama.github.io',
+  base: '/CerrajeriaDaniel',
   integrations: [react()],
-
   vite: {
     plugins: [tailwindcss()]
   }
